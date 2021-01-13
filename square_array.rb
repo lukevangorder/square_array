699ae051082c9de7cdea9e5e31345101e2af48f6
lukevangorder <lukevangorder@gmail.com>
2021-01-13 13:19:49 -0500

    commit tree fde3ec266e20ddfb9b2805ea0575800d9956fce7

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  array.each {|n|}
+  newArray=[]
+  i=0
+  while i<array.length
+    newArray[i]=array[i]*array[i]
+    i+=1
+  end
+  return newArray
 end
